cd560913c65292c88c87f0a341b679abbb5d09a8
javiscripto <javier.mecker94@gmail.com>
2024-09-26 21:10:40 -0300

    dependencias/middlewares  adiocionales

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 const express = require("express");
-const path = require("path");   
+const path = require("path"); 
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const helmet = require("helmet");
 const app = express();
 
 
@@ -10,9 +13,10 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json()); 
+app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false })); // para poder leer datos de formulario
-
-
+app.use(cors());
+app.use(helmet());
 
 const mailRouter = require("./routes/mailRouter.js");
 app.use(mailRouter);
