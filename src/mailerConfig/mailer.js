330bd7a7d5dbdb479751c6ece55113538c273f44
javiscripto <javier.mecker94@gmail.com>
2024-12-17 08:40:07 -0300

    modifico variable de entorno para deploy

--- src/mailerConfig/mailer.js
+++ src/mailerConfig/mailer.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const transporter = nodemailer.createTransport({
   service: "Gmail",
   auth: {
-    user: "javier.manque.dev@gmail.com",
+    user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS
   },
   tls: {
